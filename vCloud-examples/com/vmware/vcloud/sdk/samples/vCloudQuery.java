1d0b5ee2d7cd53a0401f10e1c753293dab396845
Charles Llewellyn <charlie.j.llewellyn@gmail.com>
2013-08-22 20:28:48 +0100

    Added vCloud Query Class

---
+++ vCloud-examples/com/vmware/vcloud/sdk/samples/vCloudQuery.java
@@ -0,0 +1,13 @@
+package com.vmware.vcloud.sdk.samples;
+
+public class vCloudQuery {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("test");
+
+	}
+
+}
